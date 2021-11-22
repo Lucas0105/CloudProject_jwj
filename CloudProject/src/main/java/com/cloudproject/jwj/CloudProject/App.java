7f85a7f452b20269f9e55f17afc69f31a9d43c20
Lucas <dnjswo105@gmail.com>
2021-11-22 18:09:03 +0900

    first

---
+++ CloudProject/src/main/java/com/cloudproject/jwj/CloudProject/App.java
@@ -0,0 +1,13 @@
+package com.cloudproject.jwj.CloudProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
